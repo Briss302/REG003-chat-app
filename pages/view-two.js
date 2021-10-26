7cd28daf6943f5ca25967cadb21e82b892d1facf
Natalia Garavito <garavitobarretonatalia@gmail.com>
2021-10-26 11:57:23 -0500

    socket real time messages back & front

--- pages/view-two.js
+++ pages/view-two.js
@@ -1,8 +1,17 @@
 import { useRouter } from 'next/router';
 import cookie from 'js-cookie';
+import {io} from 'socket.io-client';
+import { useState } from 'react';
+const socket = io("http://localhost:5000", {transports:["websocket"]});
 
 export default function Viewtwo() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
   const router = useRouter();
+
+  socket.on("send-data", (data)=>{
+    setList([...list,data]);
+  })
 
   const logOut = () => {
     cookie.remove('token');
@@ -11,38 +20,52 @@ export default function Viewtwo() {
     }
   };
 
+  const handlePost = (e) => {
+    socket.emit("run-start", {post: name});
+  };
+
   const user = cookie.get('token');
 
-  console.log('soy el token', user);
   return (
     <>
-      <div>
+    
+      <div className="h-screen flex flex-column bg-gray-900">
+        <div className="w-1/4 ">
         <button onClick={() => logOut()}>Salir</button>
-      </div>
-      <div className="h-screen flex flex-column">
-        <div className="w-1/4 "></div>
+        </div>
 
         <div className="w-1/2 flex-row">
-          <div className="w-full h-12 bg-gray-300">titulo</div>
-          <div className="w-full h-4/5 bg-gray-200">chats</div>
-          <div className="w-full h-16 flex flex-column items-center justify-center bg-gray-200">
+          <div className="w-full h-12 bg-gray-900 text-white">titulo</div>
+
+          <div className="w-full h-4/5 bg-gray-900 text-white">
+            {list.map((element, index) => {
+              return (
+                <p key={index} className="text-white">{element.post}</p>
+              )              
+            })}
+          </div>
+
+          <div className="w-full h-16 flex flex-column items-center justify-center bg-gray-900">
             <input
-              className=" h-4/5 w-4/5 border px-3 rounded-lg border-gray-400 placeholder-gray-400"
+              className=" h-4/5 w-4/5 border px-3 rounded-lg border-yellow-500 placeholder-gray-400"
               placeholder="Write your message here"
               id="inputMessage"
               name="message"
+              onChange={(e)=>setName(e.target.value)}
             />
-            <button className="h-4/5 w-1/5 m-1 p-1 rounded-xl bg-green-500 text-white flex flex-column justify-center items-center">
+            <button onClick={handlePost} className="h-4/5 w-1/5 m-1 p-1 rounded-xl border border-yellow-500 bg-gray-900 text-white flex flex-column justify-center items-center">
               <p className="p-1">Send</p>
               <img
-                src="https://cdn-icons-png.flaticon.com/512/309/309334.png"
+                src="https://cdn-icons-png.flaticon.com/512/561/561226.png"
                 className="h-7 p-1"
               ></img>
             </button>
           </div>
         </div>
 
-        <div className="w-1/4"></div>
+        <div className="w-1/4">
+          
+        </div>
       </div>
     </>
   );
--- sockets/app.js
+++ sockets/app.js
@@ -1,10 +1,15 @@
 const app = require("express");
 const server = require("http").createServer(app);
+
 const io = require("socket.io")(server,
     {cors:
-        {origin:"",methods:["GET","POST"]}});
-io.on("content", () => {
-    console.log("new user");
+        {origin:"http://localhost:3000",
+        methods:["GET","POST"]}});
+io.on("connection", (socket) => {
+    socket.on("run-start",(data)=> {
+        io.emit("send-data", data);
+        console.log(data);
+    });    
 });
-server.listen(3000);
+server.listen(5000);
 
